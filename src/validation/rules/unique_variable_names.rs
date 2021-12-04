e9b2dc1944142fe97fa8e36e9817c51cdefd1e0d
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-04 16:28:41 +0900

    unique variable name

---
+++ src/validation/rules/unique_variable_names.rs
@@ -0,0 +1,30 @@
+use std::collections::HashSet;
+
+use crate::validation::visitor::{ValidationContext, Visitor};
+
+pub struct UniqueVariableNames<'a> {
+    names: HashSet<&'a str>,
+}
+
+impl<'a> Visitor<'a> for UniqueVariableNames<'a> {
+    fn enter_operation_definition(
+        &mut self,
+        _ctx: &mut ValidationContext<'a>,
+        _operation_definition: &'a graphql_parser::query::OperationDefinition<'a, String>,
+    ) {
+        self.names.clear();
+    }
+
+    fn enter_variable_definition(
+        &mut self,
+        ctx: &mut ValidationContext,
+        variable_definition: &'a graphql_parser::query::VariableDefinition<'a, String>,
+    ) {
+        if !self.names.insert(&variable_definition.name) {
+            ctx.add_error(
+                format!("{} is already contained.", &variable_definition.name),
+                vec![variable_definition.position],
+            );
+        }
+    }
+}
