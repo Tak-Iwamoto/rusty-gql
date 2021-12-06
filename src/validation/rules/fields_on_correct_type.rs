b3693fa0e9b75cbca963c3fa7542ae9d72c9f487
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-06 13:21:56 +0900

    impl fields on correct type

--- src/validation/rules/fields_on_correct_type.rs
+++ src/validation/rules/fields_on_correct_type.rs
@@ -1,8 +1,8 @@
-use graphql_parser::query::Field;
+use graphql_parser::{query::Field, schema::TypeDefinition};
 
-use crate::validation::{
-    utils::get_type_name,
-    visitor::{ValidationContext, Visitor},
+use crate::{
+    validation::visitor::{ValidationContext, Visitor},
+    GqlTypeDefinition,
 };
 
 pub struct FieldsOnCorrectType;
@@ -10,7 +10,25 @@ pub struct FieldsOnCorrectType;
 impl<'a> Visitor<'a> for FieldsOnCorrectType {
     fn enter_field(&mut self, ctx: &mut ValidationContext, field: &'a Field<'a, String>) {
         if let Some(parent_type) = ctx.parent_type() {
-            let field_name = &field.name;
+            if matches!(
+                parent_type,
+                TypeDefinition::Union(_) | TypeDefinition::Interface(_)
+            ) {
+                if field.name == "__typename" {
+                    return;
+                }
+            }
+
+            if GqlTypeDefinition::get_field_by_name(&parent_type, &field.name).is_none() {
+                ctx.add_error(
+                    format!(
+                        "Unknown field \"{}\" on type \"{}\"",
+                        field.name,
+                        GqlTypeDefinition::type_name_from_def(parent_type)
+                    ),
+                    vec![field.position],
+                )
+            }
         }
     }
 }
