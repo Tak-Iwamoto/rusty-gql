dcbbafe13bc34dde2b7628d08230a0be0bc35374
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-13 23:54:04 +0900

    add axum example

--- examples/axum/src/main.rs
+++ examples/axum/src/main.rs
@@ -1,44 +1,68 @@
-use askama::Template;
-use rusty_gql::GraphiQLTemplate;
-use std::{convert::Infallible, net::SocketAddr};
+use rusty_gql::{execute, ArcContainer, Object, Resolver};
+use rusty_gql_axum::{GqlRequest, GqlResponse};
+use std::net::SocketAddr;
 
-use axum::{
-    body::{Bytes, Full},
-    handler::get,
-    http::{Response, StatusCode},
-    response::{Html, IntoResponse},
-    Router,
-};
+use axum::{extract::Extension, handler::post, AddExtensionLayer, Router};
+
+#[derive(Clone)]
+struct Query;
+
+#[rusty_gql::async_trait::async_trait]
+impl Resolver for Query {
+    async fn resolve_field(
+        &self,
+        ctx: &rusty_gql::FieldContext<'_>,
+    ) -> rusty_gql::ResolverResult<Option<rusty_gql::GqlValue>> {
+        Ok(None)
+    }
+}
+
+#[derive(Clone)]
+struct Mutation;
+
+#[rusty_gql::async_trait::async_trait]
+impl Resolver for Mutation {
+    async fn resolve_field(
+        &self,
+        ctx: &rusty_gql::FieldContext<'_>,
+    ) -> rusty_gql::ResolverResult<Option<rusty_gql::GqlValue>> {
+        Ok(None)
+    }
+}
+
+#[derive(Clone)]
+struct Subscription;
+
+#[rusty_gql::async_trait::async_trait]
+impl Resolver for Subscription {
+    async fn resolve_field(
+        &self,
+        ctx: &rusty_gql::FieldContext<'_>,
+    ) -> rusty_gql::ResolverResult<Option<rusty_gql::GqlValue>> {
+        Ok(None)
+    }
+}
+
+type Container = ArcContainer<Query, Mutation, Subscription>;
+
+async fn graphql_handler(container: Extension<Container>, req: GqlRequest) -> GqlResponse {
+    let result = execute(&container, req.0).await;
+    GqlResponse::from(result)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(graphiql_handler));
+    let schema_doc = std::fs::read_to_string("./src/tests/starwars.graphql").unwrap();
+    let query = Query {};
+    let mutation = Mutation {};
+    let subscription = Subscription {};
+    let container = ArcContainer::new(schema_doc.as_str(), query, mutation, subscription);
+    let app = Router::new()
+        .route("/graphql", post(graphql_handler))
+        .layer(AddExtensionLayer::new(container));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-pub async fn graphiql_handler() -> impl IntoResponse {
-    HtmlTemplate(GraphiQLTemplate {})
-}
-
-struct HtmlTemplate<T>(T);
-
-impl<T> IntoResponse for HtmlTemplate<T>
-where
-    T: Template,
-{
-    type Body = Full<Bytes>;
-    type BodyError = Infallible;
-    fn into_response(self) -> axum::http::Response<Self::Body> {
-        match self.0.render() {
-            Ok(html) => Html(html).into_response(),
-            Err(err) => Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(Full::from(format!("Failed to start GraphQL: {}", err)))
-                .unwrap(),
-        }
-    }
 }
