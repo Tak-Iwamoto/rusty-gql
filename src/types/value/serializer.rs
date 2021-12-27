3f2aa4053d0de99f1632af148405f387599cf13d
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-27 15:34:08 +0900

    add key serializer

--- src/types/value/serializer.rs
+++ src/types/value/serializer.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 use std::error::Error;
 use std::fmt::{Display, Write};
 
-use serde::ser::{self};
+use serde::ser::{self, Impossible};
 use serde_json::Number;
 
 use crate::GqlValue;
@@ -325,8 +325,8 @@ impl ser::SerializeMap for SerializeMap {
     where
         T: ser::Serialize,
     {
-        // let key = key.serialize(MapKeySerializer)?;
-        // self.key = Some(key);
+        let key = key.serialize(KeySerializer)?;
+        self.key = Some(key);
         Ok(())
     }
 
@@ -397,5 +397,186 @@ impl ser::SerializeStructVariant for SerializeStructVariant {
         let mut map = BTreeMap::new();
         map.insert(self.0, GqlValue::Object(self.1));
         Ok(GqlValue::Object(map))
+    }
+}
+
+fn cannot_serialize_except_string() -> SerializerError {
+    SerializerError("key must be a string".to_string())
+}
+struct KeySerializer;
+
+impl serde::Serializer for KeySerializer {
+    type Ok = String;
+
+    type Error = SerializerError;
+
+    type SerializeSeq = Impossible<String, SerializerError>;
+
+    type SerializeTuple = Impossible<String, SerializerError>;
+
+    type SerializeTupleStruct = Impossible<String, SerializerError>;
+
+    type SerializeTupleVariant = Impossible<String, SerializerError>;
+
+    type SerializeMap = Impossible<String, SerializerError>;
+
+    type SerializeStruct = Impossible<String, SerializerError>;
+
+    type SerializeStructVariant = Impossible<String, SerializerError>;
+
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        Ok(v.to_string())
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
+    where
+        T: serde::Serialize,
+    {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        Ok(variant.to_string())
+    }
+
+    fn serialize_newtype_struct<T: ?Sized>(
+        self,
+        _name: &'static str,
+        _value: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: serde::Serialize,
+    {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_newtype_variant<T: ?Sized>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        T: serde::Serialize,
+    {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        todo!()
+    }
+
+    fn serialize_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
+        Err(cannot_serialize_except_string())
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        Err(cannot_serialize_except_string())
     }
 }
