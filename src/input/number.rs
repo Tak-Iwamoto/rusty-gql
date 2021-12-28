a402006877ed178abd4bdaa9f563d0e8e6af5a60
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-29 00:59:27 +0900

    add float input

--- src/input/number.rs
+++ src/input/number.rs
@@ -224,11 +224,52 @@ impl GqlInputType for isize {
     }
 }
 
-// #[async_trait::async_trait]
-// impl Resolver for f64 {
-//     async fn resolve_field(&self, _ctx: &FieldContext<'_>) -> ResolverResult<Option<GqlValue>> {
-//         Ok(Some(GqlValue::Number(
-//             Number::from_f64(*self).unwrap_or_default(),
-//         )))
-//     }
-// }
+impl GqlInputType for f32 {
+    fn from_gql_value(value: Option<GqlValue>) -> Result<Self, String> {
+        match value {
+            Some(value) => match value {
+                GqlValue::Number(v) => {
+                    let n = v.as_f64().ok_or_else(|| "Invalid number".to_string())?;
+                    Ok(n as Self)
+                }
+                invalid_value => Err(format!(
+                    "Expected type: number, but found {}",
+                    invalid_value.to_string()
+                )),
+            },
+            None => Err("Expected type: number, but not found".to_string()),
+        }
+    }
+
+    fn to_gql_value(&self) -> GqlValue {
+        match Number::from_f64(*self as f64) {
+            Some(n) => GqlValue::Number(n),
+            None => GqlValue::Null,
+        }
+    }
+}
+
+impl GqlInputType for f64 {
+    fn from_gql_value(value: Option<GqlValue>) -> Result<Self, String> {
+        match value {
+            Some(value) => match value {
+                GqlValue::Number(v) => {
+                    let n = v.as_f64().ok_or_else(|| "Invalid number".to_string())?;
+                    Ok(n)
+                }
+                invalid_value => Err(format!(
+                    "Expected type: number, but found {}",
+                    invalid_value.to_string()
+                )),
+            },
+            None => Err("Expected type: number, but not found".to_string()),
+        }
+    }
+
+    fn to_gql_value(&self) -> GqlValue {
+        match Number::from_f64(*self) {
+            Some(n) => GqlValue::Number(n),
+            None => GqlValue::Null,
+        }
+    }
+}
