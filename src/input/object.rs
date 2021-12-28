36c5c3d8398f728dbd9f648aef14e034e88ccc49
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-29 01:01:13 +0900

    add hashmap input

--- src/input/object.rs
+++ src/input/object.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 
 use crate::GqlValue;
 
@@ -10,6 +10,35 @@ impl<T: GqlInputType> GqlInputType for BTreeMap<String, T> {
             Some(value) => match value {
                 GqlValue::Object(v) => {
                     let mut result = BTreeMap::new();
+                    for (key, value) in v {
+                        result.insert(key, T::from_gql_value(Some(value))?);
+                    }
+                    Ok(result)
+                }
+                invalid_value => Err(format!(
+                    "Expected type: object, but found: {}",
+                    invalid_value.to_string()
+                )),
+            },
+            None => Err("Expected type: object, but not found".to_string()),
+        }
+    }
+
+    fn to_gql_value(&self) -> GqlValue {
+        let mut result = BTreeMap::new();
+        for (key, value) in self {
+            result.insert(key.clone(), T::to_gql_value(value.clone()));
+        }
+        GqlValue::Object(result)
+    }
+}
+
+impl<T: GqlInputType> GqlInputType for HashMap<String, T> {
+    fn from_gql_value(value: Option<GqlValue>) -> Result<Self, String> {
+        match value {
+            Some(value) => match value {
+                GqlValue::Object(v) => {
+                    let mut result = HashMap::new();
                     for (key, value) in v {
                         result.insert(key, T::from_gql_value(Some(value))?);
                     }
