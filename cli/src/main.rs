131527c96e74245959cdbf2c95badca0b34c25d2
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2021-12-30 14:35:40 +0900

    add create project

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,7 @@
-use std::process;
-
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use app::build_app;
 use exit_codes::ExitCode;
+use std::process;
 
 use crate::code_generate::create_gql_files;
 
@@ -20,13 +19,21 @@ async fn run() -> Result<ExitCode> {
 
     if let Some(new_matches) = matches.subcommand_matches("new") {
         if let Some(app_name) = new_matches.value_of("name") {
-            println!("app_name: {:?}", app_name);
+            let output = process::Command::new("cargo")
+                .arg("new")
+                .arg(app_name)
+                .output();
+            if let Some(server_lib) = new_matches.value_of("lib") {
+                println!("server library: {:?}", server_lib);
+            }
+            match output {
+                Ok(_) => {
+                    println!("Successfully created the rusty-gql project!");
+                    return Ok(ExitCode::Success);
+                }
+                Err(_) => return Err(anyhow!("Failed to init rusty-gql project")),
+            }
         }
-
-        if let Some(server_lib) = new_matches.value_of("lib") {
-            println!("server library: {:?}", server_lib);
-        }
-        return Ok(ExitCode::Success);
     }
 
     Ok(ExitCode::Success)
