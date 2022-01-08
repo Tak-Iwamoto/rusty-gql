cacbdcf278aab7d1bf593d1dba427f830e9db633
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-09 00:13:10 +0900

    add fragment composite types test

--- src/validation/rules/fragments_on_composite_types.rs
+++ src/validation/rules/fragments_on_composite_types.rs
@@ -50,3 +50,149 @@ impl<'a> Visitor<'a> for FragmentsOnCompositeTypes {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::validation::test_utils::{
+        assert_fails_rule, assert_passes_rule, get_query_fragment_definitions, parse_test_query,
+        test_schema,
+    };
+
+    use super::FragmentsOnCompositeTypes;
+
+    fn factory() -> FragmentsOnCompositeTypes {
+        FragmentsOnCompositeTypes::default()
+    }
+
+    #[test]
+    fn object() {
+        let query_doc = r#"
+        fragment objectFrg on Human {
+            name
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn interface() {
+        let query_doc = r#"
+        fragment interfaceFrg on Character {
+            name
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn inline_object() {
+        let query_doc = r#"
+        fragment inlineFrg on Character {
+            ... on Human {
+                mass
+            }
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn inline_without_ty_cond() {
+        let query_doc = r#"
+        fragment withoutTy on Character {
+            ... {
+                name
+            }
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn union() {
+        let query_doc = r#"
+        fragment unionFrg on SearchResult {
+            __typename
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn not_on_scalar() {
+        let query_doc = r#"
+        fragment scalarFrg on Boolean {
+            invalid
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn not_on_enum() {
+        let query_doc = r#"
+        fragment enumFrg on LengthUnit {
+            invalid
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn not_on_input_object() {
+        let query_doc = r#"
+        fragment enumFrg on ReviewInput {
+            invalid
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn not_on_scalar_inline() {
+        let query_doc = r#"
+        fragment invalidFrg on Character {
+            ... on String {
+                name
+            }
+        }
+        { __typename }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+}
