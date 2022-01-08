123c0ccc990ab77cafbb5be86cb410ba54e3db32
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-09 00:40:41 +0900

    wip default values of correct type test

--- src/validation/rules/default_values_of_correct_type.rs
+++ src/validation/rules/default_values_of_correct_type.rs
@@ -25,3 +25,122 @@ impl<'a> Visitor<'a> for DefaultValueOfCorrectType {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::validation::test_utils::{
+        assert_fails_rule, assert_passes_rule, get_query_fragment_definitions, parse_test_query,
+        test_schema,
+    };
+
+    use super::DefaultValueOfCorrectType;
+
+    pub fn factory() -> DefaultValueOfCorrectType {
+        DefaultValueOfCorrectType
+    }
+
+    #[test]
+    fn nullable_vars_no_default_values() {
+        let query_doc = r#"
+        query NoDefaultValues($a: Int, $b: String, $c: ReviewInput) {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn required_vars_no_default_values() {
+        let query_doc = r#"
+        query RequiredValues($a: Int!, $b: String!) {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn nullable_vars_with_valid_default_values() {
+        let query_doc = r#"
+        query WithDefaultValues($a: Int = 1, $b: String = "value", $c: ReviewInput = { stars: 1 }) {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn required_vars_with_default_value() {
+        let query_doc = r#"
+        query WithRequiredDefaultValues($a: Int! = 1, $b: String! = "value") {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn nullable_vars_with_invalid_default_values() {
+        let query_doc = r#"
+        query WithInvalidValues($a: Int = "value", $b: String = 4, $c: ReviewInput = "invalidInput") {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn input_object_without_required_field() {
+        let query_doc = r#"
+        query WithoutRequiredField($a: ReviewInput = { commentary: "value" }) {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn list_vars_with_invalid_value() {
+        let query_doc = r#"
+        query InvalidList($a: [String] = ["value", 1]) {
+            hero {
+                name
+            }
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
+    }
+}
