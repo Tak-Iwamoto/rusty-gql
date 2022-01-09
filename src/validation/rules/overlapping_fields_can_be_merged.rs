2bde28e997abd659b21833c7a044897ef612ad83
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-09 14:37:49 +0900

    wip

--- src/validation/rules/overlapping_fields_can_be_merged.rs
+++ src/validation/rules/overlapping_fields_can_be_merged.rs
@@ -102,3 +102,19 @@ impl<'a> FindConflicts<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::validation::test_utils::{
+        assert_fails_rule, get_query_fragment_definitions, parse_test_query, test_schema,
+    };
+
+    use super::OverlappingFieldsCanBeMerged;
+
+    fn factory() -> OverlappingFieldsCanBeMerged {
+        OverlappingFieldsCanBeMerged
+    }
+
+    #[test]
+    fn todo() {}
+}
