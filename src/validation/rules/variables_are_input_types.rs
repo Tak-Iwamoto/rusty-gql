fac8415085e36bd0300323dbb45bf86d166b5ff8
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-09 10:24:43 +0900

    add variables are input types test

--- src/validation/rules/variables_are_input_types.rs
+++ src/validation/rules/variables_are_input_types.rs
@@ -1,6 +1,9 @@
-use crate::validation::{
-    utils::is_input_type,
-    visitor::{ValidationContext, Visitor},
+use crate::{
+    validation::{
+        utils::is_input_type,
+        visitor::{ValidationContext, Visitor},
+    },
+    GqlValueType,
 };
 
 #[derive(Default)]
@@ -12,7 +15,10 @@ impl<'a> Visitor<'a> for VariablesAreInputTypes {
         ctx: &mut ValidationContext,
         variable_definition: &'a graphql_parser::query::VariableDefinition<'a, String>,
     ) {
-        let ty = ctx.schema.type_definitions.get(&variable_definition.name);
+        let ty = ctx
+            .schema
+            .type_definitions
+            .get(GqlValueType::from(variable_definition.var_type.clone()).name());
 
         if let Some(variable_type) = ty {
             if !is_input_type(variable_type) {
@@ -26,5 +32,45 @@ impl<'a> Visitor<'a> for VariablesAreInputTypes {
                 );
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::validation::test_utils::{
+        assert_fails_rule, assert_passes_rule, get_query_fragment_definitions, parse_test_query,
+        test_schema,
+    };
+
+    use super::VariablesAreInputTypes;
+
+    fn factory() -> VariablesAreInputTypes {
+        VariablesAreInputTypes
+    }
+
+    #[test]
+    fn valid_types() {
+        let query_doc = r#"
+        query Test($a: String, $b:[Int!]!, $c: ReviewInput) {
+            test_vars(a: $a, b: $b, c: $c)
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_passes_rule(doc, schema, fragments, factory);
+    }
+
+    #[test]
+    fn invalid_types() {
+        let query_doc = r#"
+        query Test($a: Human, $b:[SearchResult!]!, $c: Character) {
+            test_vars(a: $a, b: $b, c: $c)
+        }
+        "#;
+        let schema = &test_schema();
+        let doc = &parse_test_query(query_doc);
+        let fragments = &get_query_fragment_definitions(doc, schema);
+        assert_fails_rule(doc, schema, fragments, factory);
     }
 }
