0cfd5db1e65df31ce0d0601b5227309c09d5394f
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-09 21:43:02 +0900

    fix scalar leafs validation

--- src/validation/rules/scalar_leafs.rs
+++ src/validation/rules/scalar_leafs.rs
@@ -9,7 +9,11 @@ impl<'a> Visitor<'a> for ScalarLeafs {
     fn enter_field(&mut self, ctx: &mut ValidationContext, field: &'a Field<'a, String>) {
         if let Some(parent_type) = ctx.parent_type() {
             if let Some(target_field) = parent_type.get_field_by_name(&field.name) {
-                if let Some(ty) = ctx.schema.type_definitions.get(&target_field.name) {
+                if let Some(ty) = ctx
+                    .schema
+                    .type_definitions
+                    .get(target_field.meta_type.name())
+                {
                     if ty.is_leaf_type() && !field.selection_set.items.is_empty() {
                         ctx.add_error(
                         format!("Field {} must not have a selection items because type {} has no subfields", &field.name, ty.to_string()),
@@ -106,7 +110,7 @@ mod tests {
     fn invalid_scalar_selection_on_directive_and_args() {
         let query_doc = r#"
         fragment invalidEnumField on Human {
-            friendsConnection(first: 10) @include(if: true) { test }
+            boolTest(if: false) @include(if: true) { test }
         }
         { __typename }
         "#;
