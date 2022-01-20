b55a7b8f4480fe7fcfd57fc8beb161a79b49a77c
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-21 00:00:40 +0900

    add field resolver impl trait

--- src/resolver/mod.rs
+++ src/resolver/mod.rs
@@ -5,7 +5,7 @@ mod object;
 mod optional;
 mod string;
 
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, sync::Arc};
 
 use async_trait::async_trait;
 use futures_util::{
@@ -51,6 +51,28 @@ impl<T: FieldResolver> FieldResolver for &T {
     #[allow(clippy::trivially_copy_pass_by_ref)]
     async fn resolve_field(&self, ctx: &FieldContext<'_>) -> ResolverResult<Option<GqlValue>> {
         T::resolve_field(*self, ctx).await
+    }
+    fn type_name() -> String {
+        T::type_name()
+    }
+}
+
+#[async_trait::async_trait]
+impl<T: FieldResolver> FieldResolver for Arc<T> {
+    #[allow(clippy::trivially_copy_pass_by_ref)]
+    async fn resolve_field(&self, ctx: &FieldContext<'_>) -> ResolverResult<Option<GqlValue>> {
+        T::resolve_field(self, ctx).await
+    }
+    fn type_name() -> String {
+        T::type_name()
+    }
+}
+
+#[async_trait::async_trait]
+impl<T: FieldResolver> FieldResolver for Box<T> {
+    #[allow(clippy::trivially_copy_pass_by_ref)]
+    async fn resolve_field(&self, ctx: &FieldContext<'_>) -> ResolverResult<Option<GqlValue>> {
+        T::resolve_field(self, ctx).await
     }
     fn type_name() -> String {
         T::type_name()
