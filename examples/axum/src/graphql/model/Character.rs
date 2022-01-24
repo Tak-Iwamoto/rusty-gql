a1003b3806feb0857cf465a63533234cdf4ae589
Tak-Iwamoto <rockbook0325tak178@gmail.com>
2022-01-24 23:19:40 +0900

    update axum example

--- examples/axum/src/graphql/model/Character.rs
+++ examples/axum/src/graphql/model/Character.rs
@@ -1,8 +1,18 @@
 use crate::graphql::*;
 use rusty_gql::*;
 
-#[derive(GqlUnion, Debug, Clone)]
+#[derive(GqlInterface, Debug, Clone)]
 pub enum Character {
     Human(Human),
     Droid(Droid),
+}
+
+#[GqlType(interface)]
+impl Character {
+    async fn name(&self) -> String {
+        match self {
+            Character::Human(obj) => obj.name.clone(),
+            Character::Droid(obj) => obj.name.clone(),
+        }
+    }
 }
